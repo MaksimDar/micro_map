6993ab32838be5526b8bb1966ec05b92fc9cea4f
MaksimDar <dovhusha.m@gmail.com>
2024-10-06 00:11:21 +0200

    added locations and its Marker urls

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,85 +1,10 @@
 
 
-// import React, { useEffect, useRef, useState } from 'react';
-// import mapboxgl from 'mapbox-gl';
-// import './Map.css';
-// import { getCoordinates } from '../PlaceSearch/CoordinatesStore';
-
-// mapboxgl.accessToken = 'pk.eyJ1IjoibWFrc3ltMTgiLCJhIjoiY2x5c280eDF2MDkweDJrc2N3a2R5c3l1cyJ9.70P7zxzTa0uSxIpnkz7YeA';
-
-
-// const MapBox = () => {
-//     const mapContainer = useRef(null);
-//     const mapInstance = useRef(null);
-//     const [coordinates, setCoordinates] = useState([]);
-//     const getZoom = () => {
-//         if (coordinates[0].lng == 138.2529 && coordinates[0].lat == 38.5048) {
-//             return 4
-//         } else {
-//             return 9
-//         }
-//     }
-
-//     useEffect(() => {
-//         const savedCoordinates = getCoordinates();
-//         setCoordinates(savedCoordinates);
-
-//         mapInstance.current = new mapboxgl.Map({
-//             container: mapContainer.current,
-//             style: 'mapbox://styles/mapbox/streets-v9',
-//             center: [savedCoordinates[0].lng, savedCoordinates[0].lat],
-//             zoom: savedCoordinates.length === 1 ? 9 : 4
-//         });
-
-//         mapInstance.current.addControl(new mapboxgl.NavigationControl());
-
-//         if (savedCoordinates.length > 1) {
-//             const bounds = new mapboxgl.LngLatBounds();
-//             savedCoordinates.forEach(coord => bounds.extend([coord.lng, coord.lat]));
-//             mapInstance.current.fitBounds(bounds, {
-//                 padding: 50,
-//                 essential: true
-//             });
-//         } else {
-//             mapInstance.current.flyTo({
-//                 center: [savedCoordinates[0].lng, savedCoordinates[0].lat],
-//                 zoom: 9,
-//                 essential: true
-//             });
-//         }
-
-//         return () => mapInstance.current.remove();
-//     }, []);
-
-//     useEffect(() => {
-//         if (coordinates.length > 0 && mapInstance.current) {
-//             if (coordinates.length > 1) {
-//                 const bounds = new mapboxgl.LngLatBounds();
-//                 coordinates.forEach(coord => bounds.extend([coord.lng, coord.lat]));
-//                 mapInstance.current.fitBounds(bounds, {
-//                     padding: 50,
-//                     essential: true
-//                 });
-//             } else {
-//                 mapInstance.current.flyTo({
-//                     center: [coordinates[0].lng, coordinates[0].lat],
-//                     zoom: 9,
-//                     essential: true
-//                 });
-//             }
-//         }
-//     }, [coordinates]);
-
-//     return <div ref={mapContainer} className="map-container" />;
-// };
-
-// export default MapBox;
-
-
 import { useRef, useEffect, useState } from "react";
 import { SearchBox } from "@mapbox/search-js-react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import locations from "./locations";
 
 const accessToken = "pk.eyJ1IjoibWFrc3ltMTgiLCJhIjoiY2x5c280eDF2MDkweDJrc2N3a2R5c3l1cyJ9.70P7zxzTa0uSxIpnkz7YeA";
 
@@ -99,6 +24,23 @@ export default function MapWithGeocoder() {
 
         mapInstanceRef.current.on("load", () => {
             setMapLoaded(true);
+
+            locations.forEach((location) => {
+                // Create a new marker for each location
+                const marker = new mapboxgl.Marker()
+                    .setLngLat(location.coordinates)
+                    .addTo(mapInstanceRef.current);
+
+                // Add a popup with a clickable link to Street View
+                const popup = new mapboxgl.Popup({ offset: 25 })
+                    .setHTML(`
+                        <h3>${location.name}</h3>
+                        <p><a href="${location.streetViewLink}" target="_blank">View in ${location.name}</a></p>
+                    `);
+
+                // Associate the popup with the marker
+                marker.setPopup(popup);
+            });
         });
     }, []);
 
