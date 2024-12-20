2881aceb5ab737d6eeb9744381d83dac9e528310
MaksimDar <dovhusha.m@gmail.com>
2024-12-20 21:13:10 +0100

    basic changes

--- src/components/Map/locations.js
+++ src/components/Map/locations.js
@@ -1,36 +1,36 @@
 // locations.js
 
-const locations = [
-    {
-        name: "Toranomon 1",
-        coordinates: [139.744618, 35.6694842],
-        streetViewLink: "https://www.google.com/maps/@35.6687439,139.7477481,3a,63.4y,101.85h,83.64t/data=!3m7!1e1!3m5!1sMTX-1LF3xHW-1qw7OQzsFQ!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DMTX-1LF3xHW-1qw7OQzsFQ%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D63.363056%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192?entry=ttu%27,"
-    },
-    {
-        name: "Toranomon 2",
-        coordinates: [139.7457219, 35.6680353],
-        streetViewLink: "https://www.google.com/maps/@35.6688498,139.7441341,3a,75y,49.65h,89.89t/data=!3m7!1e1!3m5!1srMjfw6c7G_M3yMVUPh2y8w!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DrMjfw6c7G_M3yMVUPh2y8w%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D110.13482%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192?entry=ttu',"
-    },
-    {
-        name: "Toranomon 3",
-        coordinates: [139.7447969, 35.6664651],
-        streetViewLink: "https://www.google.com/maps/@35.6686903,139.746472,3a,28.6y,118.04h,92.84t/data=!3m7!1e1!3m5!1sBy700R0QpErGyYlG8_WtEA!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DBy700R0QpErGyYlG8_WtEA%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D19.13151%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192?entry=ttu',"
-    },
-    {
-        name: "Toranomon 4",
-        coordinates: [139.7465624, 35.6672006],
-        streetViewLink: "https://www.google.com/maps/@35.6666443,139.7451046,3a,87.3y,155.22h,88.29t/data=!3m6!1e1!3m4!1s9XXqpRE8Eu6aXydHfYFnig!2e0!7i16384!8i8192"
-    },
-    {
-        name: "Toranomon 5",
-        coordinates: [139.7482612, 35.6685914],
-        streetViewLink: "https://www.google.com/maps/@35.6664651,139.7447969,3a,68.9y,125.38h,85.06t/data=!3m6!1e1!3m4!1sA8EKqrPLXzDQ9fvQ2JQXFQ!2e0!7i16384!8i8192"
-    },
-    {
-        name: "Toranomon 6",
-        coordinates: [139.7466774, 35.6688638],
-        streetViewLink: "https://www.google.com/maps/@35.6680353,139.7457219,3a,23.1y,321.51h,87.1t/data=!3m6!1e1!3m4!1sbobHAsAxAsreBPsf_Gkddw!2e0!7i16384!8i8192"
-    }
-];
+// const locations = [
+//     {
+//         name: "Toranomon 1",
+//         coordinates: [139.744618, 35.6694842],
+//         streetViewLink: "https://www.google.com/maps/@35.6687439,139.7477481,3a,63.4y,101.85h,83.64t/data=!3m7!1e1!3m5!1sMTX-1LF3xHW-1qw7OQzsFQ!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DMTX-1LF3xHW-1qw7OQzsFQ%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D63.363056%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192?entry=ttu%27,"
+//     },
+//     {
+//         name: "Toranomon 2",
+//         coordinates: [139.7457219, 35.6680353],
+//         streetViewLink: "https://www.google.com/maps/@35.6688498,139.7441341,3a,75y,49.65h,89.89t/data=!3m7!1e1!3m5!1srMjfw6c7G_M3yMVUPh2y8w!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DrMjfw6c7G_M3yMVUPh2y8w%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D110.13482%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192?entry=ttu',"
+//     },
+//     {
+//         name: "Toranomon 3",
+//         coordinates: [139.7447969, 35.6664651],
+//         streetViewLink: "https://www.google.com/maps/@35.6686903,139.746472,3a,28.6y,118.04h,92.84t/data=!3m7!1e1!3m5!1sBy700R0QpErGyYlG8_WtEA!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DBy700R0QpErGyYlG8_WtEA%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D19.13151%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192?entry=ttu',"
+//     },
+//     {
+//         name: "Toranomon 4",
+//         coordinates: [139.7465624, 35.6672006],
+//         streetViewLink: "https://www.google.com/maps/@35.6666443,139.7451046,3a,87.3y,155.22h,88.29t/data=!3m6!1e1!3m4!1s9XXqpRE8Eu6aXydHfYFnig!2e0!7i16384!8i8192"
+//     },
+//     {
+//         name: "Toranomon 5",
+//         coordinates: [139.7482612, 35.6685914],
+//         streetViewLink: "https://www.google.com/maps/@35.6664651,139.7447969,3a,68.9y,125.38h,85.06t/data=!3m6!1e1!3m4!1sA8EKqrPLXzDQ9fvQ2JQXFQ!2e0!7i16384!8i8192"
+//     },
+//     {
+//         name: "Toranomon 6",
+//         coordinates: [139.7466774, 35.6688638],
+//         streetViewLink: "https://www.google.com/maps/@35.6680353,139.7457219,3a,23.1y,321.51h,87.1t/data=!3m6!1e1!3m4!1sbobHAsAxAsreBPsf_Gkddw!2e0!7i16384!8i8192"
+//     }
+// ];
 
-export default locations;
+// export default locations;
